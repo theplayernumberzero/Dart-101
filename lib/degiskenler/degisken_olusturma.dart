9658ee07f1baf25a7c8aa1dd50b64a3a63db3e13
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-03-26 02:43:30 +0300

    lesson 1

---
+++ lib/degiskenler/degisken_olusturma.dart
@@ -0,0 +1,13 @@
+void main(){
+  var ogrenciAdi = "Bahadir";
+  var ogrenciYas = 22;
+  var ogrenciBoy = 1.80;
+  var ogrenciBasharf = 'B';
+  var ogrenciDevamDurumu = true;
+
+  print(ogrenciAdi);
+  print(ogrenciYas);
+  print(ogrenciBoy);
+  print(ogrenciBasharf);
+  print(ogrenciDevamDurumu);
+}
